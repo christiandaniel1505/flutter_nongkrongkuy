0e43728b18a1fdfbd0adeca8205eaa02fe4d8094
Christian Daniel Prayogo <danielchristian1505@gmail.com>
2021-12-31 19:30:29 +0700

    add auth service

---
+++ lib/pages/home/home.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_nongkrongkuy/models/user.dart';
+import 'package:flutter_nongkrongkuy/services/auth.dart';
+import 'package:provider/provider.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'dart:io';
+
+class HomeMain extends StatefulWidget {
+  HomeMain({Key? key}) : super(key: key);
+
+  @override
+  _HomeMainState createState() => _HomeMainState();
+}
+
+class _HomeMainState extends State<HomeMain> {
+  PageController pageController = new PageController();
+  bool isTest = false;
+  User user = new User();
+
+  @override
+  void initState() {
+    super.initState();
+    pageController = PageController(viewportFraction: 6.0);
+    getUser();
+  }
+
+  void getUser() async {
+    final String uri = "https://stulish-rest-api.herokuapp.com/api/v1/user";
+    String? token =
+        await Provider.of<AuthProvider>(context, listen: false).getToken();
+    http.Response result = await http.get(Uri.parse(uri), headers: {
+      'Authorization': 'Bearer $token',
+    });
+    if (result.statusCode == HttpStatus.ok) {
+      final jsonResponse = json.decode(result.body);
+      var users = User.toString(jsonResponse);
+      setState(() {
+        user = users;
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Color(0xFF007251),
+        body: SingleChildScrollView(
+          child: Container(
+            child: Padding(
+              padding: const EdgeInsets.only(top: 50, right: 20, left: 20),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/pages/login/login.dart
@@ -0,0 +1,137 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_nongkrongkuy/services/auth.dart';
+import 'package:provider/provider.dart';
+
+class LoginMain extends StatefulWidget {
+  LoginMain({Key? key}) : super(key: key);
+
+  @override
+  _LoginMainState createState() => _LoginMainState();
+}
+
+class _LoginMainState extends State<LoginMain> {
+  final _formKey = GlobalKey<FormState>();
+  TextEditingController emailController = new TextEditingController();
+  TextEditingController passwordController = new TextEditingController();
+  String _errorMessage = "";
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFF007251),
+      body: SingleChildScrollView(
+        child: Container(
+            child: Center(
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(top: 40, bottom: 20),
+                child: Text("Login Page",
+                    style: TextStyle(color: Colors.white, fontSize: 20)),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(bottom: 40),
+                child: Text(
+                  "NongkrongKuy",
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 40,
+                      fontWeight: FontWeight.bold),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(
+                    top: 10, bottom: 10, right: 20, left: 20),
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.only(left: 10, right: 10),
+                    child: TextFormField(
+                      controller: emailController,
+                      decoration: InputDecoration(
+                        labelText: 'Email',
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(
+                    top: 10, bottom: 10, right: 20, left: 20),
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.only(left: 10, right: 10),
+                    child: TextFormField(
+                      controller: passwordController,
+                      obscureText: true,
+                      decoration: InputDecoration(
+                        labelText: 'Password',
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(0.0, 16.0, 0.0, 0.0),
+                child: Text(
+                  _errorMessage,
+                  style: TextStyle(color: Colors.red),
+                ),
+              ),
+              InkWell(
+                onTap: () async {
+                  setState(() {
+                    _errorMessage = "";
+                  });
+                  bool result =
+                      await Provider.of<AuthProvider>(context, listen: false)
+                          .login(emailController.text, passwordController.text);
+                  if (result == false) {
+                    setState(() {
+                      _errorMessage = 'Email or Password Wrong';
+                    });
+                  }
+                },
+                child: Padding(
+                  padding: const EdgeInsets.only(
+                      top: 10, bottom: 10, right: 20, left: 20),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Color(0xFFFFD900),
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.only(
+                          top: 15, bottom: 15, left: 30, right: 30),
+                      child: Text(
+                        "Login",
+                        style: TextStyle(
+                            color: Color(0xFF007251),
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20),
+                      ),
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
+        )),
+      ),
+    );
+  }
+}
